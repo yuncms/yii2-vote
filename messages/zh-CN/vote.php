23bead2fd59afd2b32355bf4357b443cecb3b1ca
xuchao <85825770@qq.com>
2017-03-13 10:03:35 +0800

    增加中文语言包

---
+++ messages/zh-CN/vote.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link https://github.com/Chiliec/yii2-vote
+ * @author Vladimir Babin <vovababin@gmail.com>
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+return [
+    'The user is not recognized' => '用户无法识别',
+    'Guests are not allowed to vote' => '游客无法参与投票',
+    'The model is not registered' => '模型未注册!',
+    'The purpose is not defined' => '未定义',
+    'Wrong action' => '非法操作!',
+    'Your vote is accepted. Thanks!' => '投票完成!',
+    'Your vote has been changed. Thanks!' => '已更新!',
+    'Thanks for your opinion' => '谢谢你的意见',
+    'Validation error' => '验证错误',
+    'You have already voted!' => '已经投过票!',
+    'Forbidden method' => '禁止方法',
+    'Aggregate rating' => '总评分',
+    'modelId has not been sent' => 'ID尚未发送',
+    'Target id has not been sent' => '目标ID尚未发送',
+    'Model not configurated' => '未配置模型',
+];
